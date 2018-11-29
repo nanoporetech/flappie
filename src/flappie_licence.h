f5a84ddd0189b2992092cb6d1d4687f175fd6fb3
Tim Massingham <tim.massingham@nanoporetech.com>
2018-11-29 00:27:19 +0000

    Print licence information on --licence

--- src/flappie_licence.h
+++ src/flappie_licence.h
@@ -13,6 +13,10 @@
 static const char *flappie_licence_text =
     "(c) 2018 Oxford Nanopore Technologies Ltd.\n"
     "\n"
+    "This Source Code Form is subject to the terms of the Oxford Nanopore\n"
+    "Technologies, Ltd. Public License, v. 1.0. If a copy of the License\n"
+    "was not  distributed with this file, You can obtain one at\n"
+    "http://nanoporetech.com\n"
     "\n"
     "\nThe vectorised math functions 'src/sse_mathfun.h' are from\n"
     "http://gruntthepeon.free.fr/ssemath/ and the original version of this file is\n"
